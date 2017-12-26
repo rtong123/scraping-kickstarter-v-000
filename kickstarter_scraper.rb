a71190477c431baf6100afc5124590f136847102
Rebecca Tong <rtong@buffalo.edu>
2017-12-26 20:41:13 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,10 +5,9 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
-
-  kickstarter = Nokogiri::HTML(html)
-end
-
 binding.pry
+  kickstarter = Nokogiri::HTML(html)
+
+end
 
 create_project_hash
